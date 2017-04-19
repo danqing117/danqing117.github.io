3119aad23ea853568b6d9d61855e463c5712023a
danqing117 <danqingyin1992@gmail.com>
2017-04-19 17:19:24 -0300

    Update post.js

--- js/post.js
+++ js/post.js
@@ -3,7 +3,7 @@ $(function() {
 
     window.disqus_shortname = 'beiyuu'; // required: replace example with your forum shortname
     $('#disqus_container .comment').on('click',function(){
-        $(this).html('加载中...');
+        $(this).html('loading');
         var that = this;
         $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
     });
